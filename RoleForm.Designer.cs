c394460e79acfd35b9396b1c8e518dc6ef52a153
José Jorge <J.jorge@motorcredito.com.do>
2021-09-18 18:10:37 -0400

    Add project files.

---
+++ RoleForm.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace DS1
+{
+    partial class RoleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RoleForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDescFilter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitleFilter = new System.Windows.Forms.TextBox();
+            this.dgvRoles = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnSaveRole = new System.Windows.Forms.Button();
+            this.rbDisabled = new System.Windows.Forms.RadioButton();
+            this.rbEnabled = new System.Windows.Forms.RadioButton();
+            this.btnCancelEdit = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtDescRol = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTituloRol = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRoles)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.btnFilter);
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtDescFilter);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtTitleFilter);
+            this.groupBox1.Location = new System.Drawing.Point(284, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(308, 224);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PANEL DE BUSQUEDA DE ROL";
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFilter.Location = new System.Drawing.Point(21, 138);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(75, 23);
+            this.btnFilter.TabIndex = 23;
+            this.btnFilter.Text = "FILTRAR";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(102, 138);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 22;
+            this.btnCancel.Text = "Cancelar";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Descripcion:";
+            // 
+            // txtDescFilter
+            // 
+            this.txtDescFilter.Location = new System.Drawing.Point(21, 99);
+            this.txtDescFilter.Name = "txtDescFilter";
+            this.txtDescFilter.Size = new System.Drawing.Size(268, 20);
+            this.txtDescFilter.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Titulo:";
+            // 
+            // txtTitleFilter
+            // 
+            this.txtTitleFilter.Location = new System.Drawing.Point(21, 49);
+            this.txtTitleFilter.Name = "txtTitleFilter";
+            this.txtTitleFilter.Size = new System.Drawing.Size(268, 20);
+            this.txtTitleFilter.TabIndex = 18;
+            // 
+            // dgvRoles
+            // 
+            this.dgvRoles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRoles.Location = new System.Drawing.Point(12, 242);
+            this.dgvRoles.Name = "dgvRoles";
+            this.dgvRoles.Size = new System.Drawing.Size(580, 351);
+            this.dgvRoles.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label16);
+            this.groupBox2.Controls.Add(this.label12);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.btnSaveRole);
+            this.groupBox2.Controls.Add(this.rbDisabled);
+            this.groupBox2.Controls.Add(this.rbEnabled);
+            this.groupBox2.Controls.Add(this.btnCancelEdit);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.txtDescRol);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.txtTituloRol);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(266, 224);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "GESTION DE ROLES";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.ForeColor = System.Drawing.Color.Red;
+            this.label16.Location = new System.Drawing.Point(57, 138);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(11, 13);
+            this.label16.TabIndex = 26;
+            this.label16.Text = "*";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.ForeColor = System.Drawing.Color.Red;
+            this.label12.Location = new System.Drawing.Point(75, 83);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(11, 13);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "*";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.Color.Red;
+            this.label11.Location = new System.Drawing.Point(79, 33);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(11, 13);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "*";
+            // 
+            // btnSaveRole
+            // 
+            this.btnSaveRole.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnSaveRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveRole.Location = new System.Drawing.Point(16, 189);
+            this.btnSaveRole.Name = "btnSaveRole";
+            this.btnSaveRole.Size = new System.Drawing.Size(158, 23);
+            this.btnSaveRole.TabIndex = 18;
+            this.btnSaveRole.Text = "GUARDAR CAMBIOS";
+            this.btnSaveRole.UseVisualStyleBackColor = false;
+            this.btnSaveRole.Click += new System.EventHandler(this.btnSaveEdit_Click);
+            // 
+            // rbDisabled
+            // 
+            this.rbDisabled.AutoSize = true;
+            this.rbDisabled.Location = new System.Drawing.Point(82, 157);
+            this.rbDisabled.Name = "rbDisabled";
+            this.rbDisabled.Size = new System.Drawing.Size(63, 17);
+            this.rbDisabled.TabIndex = 21;
+            this.rbDisabled.Text = "Inactivo";
+            this.rbDisabled.UseVisualStyleBackColor = true;
+            // 
+            // rbEnabled
+            // 
+            this.rbEnabled.AutoSize = true;
+            this.rbEnabled.Checked = true;
+            this.rbEnabled.Location = new System.Drawing.Point(21, 156);
+            this.rbEnabled.Name = "rbEnabled";
+            this.rbEnabled.Size = new System.Drawing.Size(55, 17);
+            this.rbEnabled.TabIndex = 20;
+            this.rbEnabled.TabStop = true;
+            this.rbEnabled.Text = "Activo";
+            this.rbEnabled.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelEdit
+            // 
+            this.btnCancelEdit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelEdit.Location = new System.Drawing.Point(180, 189);
+            this.btnCancelEdit.Name = "btnCancelEdit";
+            this.btnCancelEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelEdit.TabIndex = 17;
+            this.btnCancelEdit.Text = "Cancelar";
+            this.btnCancelEdit.UseVisualStyleBackColor = true;
+            this.btnCancelEdit.Click += new System.EventHandler(this.btnCancelEdit_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(18, 138);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Estado:";
+            // 
+            // txtDescRol
+            // 
+            this.txtDescRol.Location = new System.Drawing.Point(17, 99);
+            this.txtDescRol.Name = "txtDescRol";
+            this.txtDescRol.Size = new System.Drawing.Size(216, 20);
+            this.txtDescRol.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(14, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Descripcion:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Titulo de Rol:";
+            // 
+            // txtTituloRol
+            // 
+            this.txtTituloRol.Location = new System.Drawing.Point(17, 49);
+            this.txtTituloRol.Name = "txtTituloRol";
+            this.txtTituloRol.Size = new System.Drawing.Size(216, 20);
+            this.txtTituloRol.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(241, 177);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(67, 47);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // RoleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 605);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dgvRoles);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "RoleForm";
+            this.Text = "RoleForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRoles)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescFilter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitleFilter;
+        private System.Windows.Forms.DataGridView dgvRoles;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnSaveRole;
+        private System.Windows.Forms.RadioButton rbDisabled;
+        private System.Windows.Forms.RadioButton rbEnabled;
+        private System.Windows.Forms.Button btnCancelEdit;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtDescRol;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTituloRol;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ RoleForm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DS1
+{
+    public partial class RoleForm : Form
+    {
+        DS1Entities db = new DS1Entities();
+        List<string> msg = new List<string>();
+        public RoleForm()
+        {
+            InitializeComponent();
+
+            LoadRoles();
+        }
+
+        void LoadRoles()
+        {
+            var roles = db.Role.ToList();
+
+            if (!string.IsNullOrEmpty(txtTitleFilter.Text))
+            {
+                roles = roles.Where(x => x.Title.Contains(txtTitleFilter.Text)).ToList();
+            }
+
+            if (!string.IsNullOrEmpty(txtDescFilter.Text))
+            {
+                roles = roles.Where(x => x.Description.Contains(txtDescFilter.Text)).ToList();
+            }
+
+            dgvRoles.DataSource = roles.ToList();
+        }
+
+        private void btnSaveEdit_Click(object sender, EventArgs e)
+        {
+            if (ValidateSaving() == true)
+            {
+                Save();
+            }
+            else
+            {
+                var list = string.Empty;
+                foreach (var item in msg)
+                {
+                    list += item + "\n";
+                }
+
+                MessageBox.Show(list, "INTEC");
+            }
+        }
+        bool ValidateSaving()
+        {
+            msg = new List<string>();
+            bool result = true;
+
+            if (string.IsNullOrEmpty(txtTituloRol.Text))
+            {
+                msg.Add("El Rol es Requerido.");
+                result = false;
+            }
+
+            //Validations in DB
+            if (CheckTitle(txtTituloRol.Text))
+            {
+                msg.Add($"El Rol de Usuario ({txtTituloRol.Text.ToUpper()}) ya esta registrado");
+                result = false;
+            }
+
+            return result;
+        }
+        bool CheckTitle(string titulo)
+        {
+            return db.Role.Count(x => x.Title == titulo) > 0;
+        }
+
+        void Save()
+        {
+            try
+            {
+                var rol = new Role
+                {
+                    Title = txtTituloRol.Text,
+                    Description = txtDescRol.Text,
+                    Enabled = rbEnabled.Checked,
+                    CreatedDate = DateTime.Now
+                };
+
+                db.Role.Add(rol);
+                bool result = db.SaveChanges() > 0;
+
+                if (result)
+                {
+                    LoadRoles();
+
+                    ClearForm();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: Saving Role");
+                Console.WriteLine(ex);
+            }
+        }
+        void ClearForm()
+        {
+            txtTituloRol.Text = string.Empty;
+            txtDescRol.Text = string.Empty;
+            rbEnabled.Checked = true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtTitleFilter.Text = string.Empty;
+            txtDescFilter.Text = string.Empty;
+        }
+
+        private void btnCancelEdit_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            LoadRoles();
+        }
+    }
+}
